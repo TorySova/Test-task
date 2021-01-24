b3f52ce91d4414e138eaada770e552f14d5c4ad2
TorySova <toryssova@gmail.com>
2021-01-24 16:21:31 +0200

    add media css

--- src/components/Profile/Structure/Structure.tsx
+++ src/components/Profile/Structure/Structure.tsx
@@ -4,22 +4,24 @@ import str from '../../../common/img/str.png'
 
 export const Structure = () => {
     return (
-        <div className={s.structure}>
-            <div className={s.item} >
-                <img src={str} alt="boxImg" />
-                <div>Блок 1</div>
-            </div>
-            <div className={s.item}>
-                <img src={str} alt="boxImg" />
-                <div>Блок 2</div>
-            </div>
-            <div className={s.item}>
-                <img src={str} alt="boxImg" />
-                <div>Блок 3</div>
-            </div>
-            <div className={s.item}>
-                <img src={str} alt="boxImg" />
-                <div>Блок 4</div>
+        <div className={s.structureWrap}>
+            <div className={s.struct}>
+                <div className={s.item} >
+                    <img src={str} alt="boxImg" />
+                    <div>Блок 1</div>
+                </div>
+                <div className={s.item}>
+                    <img src={str} alt="boxImg" />
+                    <div>Блок 2</div>
+                </div>
+                <div className={s.item}>
+                    <img src={str} alt="boxImg" />
+                    <div>Блок 3</div>
+                </div>
+                <div className={s.item}>
+                    <img src={str} alt="boxImg" />
+                    <div>Блок 4</div>
+                </div>
             </div>
         </div>
     )
